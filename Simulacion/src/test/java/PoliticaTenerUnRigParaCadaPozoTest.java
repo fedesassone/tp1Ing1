41b01be7094f0c173813cbc7095a73a405a1f7e5
Federico <fede.sassone@hotmail.com>
2017-05-27 20:04:27 -0300

    Politica Un rig para cada Pozo

---
+++ Simulacion/src/test/java/PoliticaTenerUnRigParaCadaPozoTest.java
@@ -0,0 +1,45 @@
+
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+class PoliticaTenerUnRigParaCadaPozoTest {
+    @Test
+    void testAplicarPolitica() {
+        //Setup de politica, pozos, plantas y tanques
+        int numeroPlantas = 2;
+        int numeroTanquesGas = 1;
+        int numeroTanquesAgua = 1;
+
+        List<PozoEnExcavacion> pozosEnExcavacion = new LinkedList<PozoEnExcavacion>();
+        pozosEnExcavacion.add(new PozoEnExcavacion(200));
+        pozosEnExcavacion.add(new PozoEnExcavacion(300));
+
+        ReguladorPozo reguladorPozo = new ReguladorPozo(new LinkedList<Pozo>(), pozosEnExcavacion);
+
+        ReguladorPlantaSeparadora reguladorPlantaSeparadora = new Fixtures().reguladorPlantaCon(numeroPlantas);
+
+        ReguladorTanque reguladorTanqueGas = new Fixtures().reguladorTanqueCon(numeroTanquesGas, TipoDeProducto.GAS);
+        ReguladorTanque reguladorTanqueAgua = new Fixtures().reguladorTanqueCon(numeroTanquesAgua, TipoDeProducto.AGUA);
+
+        Reservorio reservorio = new Reservorio(0.2, 0.3, 0.5, 1000);
+        Logger logger = new Logger();
+
+
+        Simulador simulador = new Simulador(reguladorTanqueAgua, reguladorTanqueGas, reguladorPozo,
+                reguladorPlantaSeparadora, reservorio, logger);
+
+        PoliticaCompraDeRIGs politicaCompraDeRIGs = new PoliticaTenerUnRigParaCadaPozo();
+
+        politicaCompraDeRIGs.aplicarPolitica(simulador);
+
+        assert (simulador.rigsAlquilados.size() == 2 );
+
+
+
+
+    }
+}
