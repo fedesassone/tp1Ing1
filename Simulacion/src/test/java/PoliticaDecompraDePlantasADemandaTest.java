315383a6d7400eb89332c29edf2b860cc6d506ec
ntallar <nicolastallar@gmail.com>
2017-05-27 16:10:37 -0300

    [FIX] Arregle compilacion test de politica de compras

--- Simulacion/src/test/java/PoliticaDecompraDePlantasADemandaTest.java
+++ Simulacion/src/test/java/PoliticaDecompraDePlantasADemandaTest.java
@@ -24,8 +24,8 @@ public class PoliticaDecompraDePlantasADemandaTest {
         double b = reguladorPozo.capacidadDeExtraccionTotal(3);
         double a = reguladorPlantaSeparadora.capacidadDeSeparacionTotal();
         //double b = reguladorPozo.cant
-        ReguladorTanque reguladorTanqueGas = new Fixtures().reguladorTanqueCon(numeroTanquesGas);
-        ReguladorTanque reguladorTanqueAgua = new Fixtures().reguladorTanqueCon(numeroTanquesAgua);
+        ReguladorTanque reguladorTanqueGas = new Fixtures().reguladorTanqueCon(numeroTanquesGas, TipoDeProducto.GAS);
+        ReguladorTanque reguladorTanqueAgua = new Fixtures().reguladorTanqueCon(numeroTanquesAgua, TipoDeProducto.AGUA);
         Reservorio reservorio = new Reservorio(0.2, 0.3, 0.5, 1000);
         Logger logger = new Logger();
         Simulador simulador = new Simulador(reguladorTanqueAgua, reguladorTanqueGas, reguladorPozo,
@@ -33,9 +33,9 @@ public class PoliticaDecompraDePlantasADemandaTest {
         PoliticaComprarPlantasADemanda politicaCompra = new PoliticaComprarPlantasADemanda(logger);
         politicaCompra.aplicarPolitica(simulador);
         int cantidadFinal = reguladorPlantaSeparadora.numeroDePlantasEnConstruccion();
-        //assert(cantidadFinal>cantidadInicial);
-        //assert(reguladorPlantaSeparadora.futuraCapacidadDeSeparacionTotal() >= reguladorPozo.capacidadDeExtraccionTotal(3));
-        //assert(reguladorPlantaSeparadora.futuraCapacidadDeSeparacionTotal() >= reguladorPozo.capacidadDeExtraccionTotal(5));
+        assert(cantidadFinal>cantidadInicial);
+        assert(reguladorPlantaSeparadora.futuraCapacidadDeSeparacionTotal() >= reguladorPozo.capacidadDeExtraccionTotal(3));
+        assert(reguladorPlantaSeparadora.futuraCapacidadDeSeparacionTotal() >= reguladorPozo.capacidadDeExtraccionTotal(5));
 
     }
 
@@ -59,8 +59,8 @@ public class PoliticaDecompraDePlantasADemandaTest {
         double b = reguladorPozo.capacidadDeExtraccionTotal(1);
         double a = reguladorPlantaSeparadora.capacidadDeSeparacionTotal();
         //double b = reguladorPozo.cant
-        ReguladorTanque reguladorTanqueGas = new Fixtures().reguladorTanqueCon(numeroTanquesGas);
-        ReguladorTanque reguladorTanqueAgua = new Fixtures().reguladorTanqueCon(numeroTanquesAgua);
+        ReguladorTanque reguladorTanqueGas = new Fixtures().reguladorTanqueCon(numeroTanquesGas, TipoDeProducto.GAS);
+        ReguladorTanque reguladorTanqueAgua = new Fixtures().reguladorTanqueCon(numeroTanquesAgua, TipoDeProducto.AGUA);
         Reservorio reservorio = new Reservorio(0.2, 0.3, 0.5, 1000);
         Logger logger = new Logger();
         Simulador simulador = new Simulador(reguladorTanqueAgua, reguladorTanqueGas, reguladorPozo,
@@ -70,7 +70,7 @@ public class PoliticaDecompraDePlantasADemandaTest {
         int cantidadFinal = reguladorPlantaSeparadora.numeroDePlantasEnConstruccion();
         double c = reguladorPlantaSeparadora.futuraCapacidadDeSeparacionTotal();
 
-        //assert(reguladorPlantaSeparadora.futuraCapacidadDeSeparacionTotal() >= reguladorPozo.capacidadDeExtraccionTotal(3));
-        //assert(reguladorPlantaSeparadora.futuraCapacidadDeSeparacionTotal() >= reguladorPozo.capacidadDeExtraccionTotal(5));
+        assert(reguladorPlantaSeparadora.futuraCapacidadDeSeparacionTotal() >= reguladorPozo.capacidadDeExtraccionTotal(3));
+        assert(reguladorPlantaSeparadora.futuraCapacidadDeSeparacionTotal() >= reguladorPozo.capacidadDeExtraccionTotal(5));
     }
 }
